0a5faa410dfc422d8e63a7f2e756579d63511b2d
Clark Sanford <clrksanford@gmail.com>
2016-11-28 13:16:50 -0600

    Add provider and store to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 import firebase from './utils/firebase';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 // Components
 import App from './App';
@@ -13,16 +15,23 @@ import NewTripModal from './components/NewTripModal';
 import CompletedTripPage from './components/CompletedTripPage';
 import Destinations from './components/Destinations';
 
+// Reducers and actions
+import reducer from './reducers'
+
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path='/' component={App} firebase={firebase}>
-      <IndexRoute component={Home} firebase={firebase}/>
-      <Route path='/profile' component={Profile}/>
-      <Route path='/planner/:uid/:tripId/:destination' component={TripPlanningPage}/>
-      <Route path='/newTrip' component={NewTripModal}/>
-      <Route path='/destinations' component={Destinations}/>
-      <Route path='/completed/:uid/:tripId/:destination' component={CompletedTripPage}/>
-    </Route>
-  </Router>,
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path='/' component={App} firebase={firebase}>
+        <IndexRoute component={Home} firebase={firebase}/>
+        <Route path='/profile' component={Profile}/>
+        <Route path='/planner/:uid/:tripId/:destination' component={TripPlanningPage}/>
+        <Route path='/newTrip' component={NewTripModal}/>
+        <Route path='/destinations' component={Destinations}/>
+        <Route path='/completed/:uid/:tripId/:destination' component={CompletedTripPage}/>
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );

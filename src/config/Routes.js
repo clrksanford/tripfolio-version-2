4a1d296955816eab52d105e91812d75bc7268f6c
Clark Sanford <clrksanford@gmail.com>
2016-12-02 13:40:11 -0600

    Move route configuration to separate file

---
+++ src/config/Routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+import firebase from '../utils/firebase';
+
+// Components
+import App from '../App';
+import Home from '../components/Home';
+import Profile from '../components/Profile';
+import TripPlanningPage from '../components/TripPlanningPage';
+import NewTripModal from '../components/NewTripModal';
+import CompletedTripPage from '../components/CompletedTripPage';
+import Destinations from '../components/Destinations';
+
+
+export default () => (
+  <Router history={hashHistory}>
+    <Route path='/' component={App} firebase={firebase}>
+      <IndexRoute component={Home} firebase={firebase}/>
+      <Route path='/profile' component={Profile}/>
+      <Route path='/planner/:uid/:tripId/:destination' component={TripPlanningPage}/>
+      <Route path='/newTrip' component={NewTripModal}/>
+      <Route path='/destinations' component={Destinations}/>
+      <Route path='/completed/:uid/:tripId/:destination' component={CompletedTripPage}/>
+    </Route>
+  </Router>
+)
--- src/index.js
+++ src/index.js
@@ -1,40 +1,30 @@
+/*---BEGIN IMPORTS---*/
+
 // Modules
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory, IndexRoute} from 'react-router';
-import firebase from './utils/firebase';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createLogger from 'redux-logger';
 
 // Components
-import App from './App';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import TripPlanningPage from './components/TripPlanningPage';
-import NewTripModal from './components/NewTripModal';
-import CompletedTripPage from './components/CompletedTripPage';
-import Destinations from './components/Destinations';
+import ConfigRoutes from './config/Routes';
 
 // Reducers and actions
 import reducer from './reducers'
 
+/*---END IMPORTS---*/
+
+
+// Configure redux middleware
 const logger = createLogger();
 const initialState = {};
 const store = createStore(reducer, initialState, applyMiddleware(logger));
 
+
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path='/' component={App} firebase={firebase}>
-        <IndexRoute component={Home} firebase={firebase}/>
-        <Route path='/profile' component={Profile}/>
-        <Route path='/planner/:uid/:tripId/:destination' component={TripPlanningPage}/>
-        <Route path='/newTrip' component={NewTripModal}/>
-        <Route path='/destinations' component={Destinations}/>
-        <Route path='/completed/:uid/:tripId/:destination' component={CompletedTripPage}/>
-      </Route>
-    </Router>
+    <ConfigRoutes />
   </Provider>,
   document.getElementById('root')
 );

ae39bd700e5e35f62cb60a6800f9bdffeefb3bae
Clark Sanford <clrksanford@gmail.com>
2016-12-04 14:34:32 -0600

    Remove console.log

--- src/actions/getSelectedTrip.js
+++ src/actions/getSelectedTrip.js
@@ -6,7 +6,6 @@ const getSelectedTrip = (tripId) => {
     axios.get(`https://lit-garden-98394.herokuapp.com/trips/${tripId}`)
       .then((response) => {
         let trip = response.data;
-        console.log(trip);
 
         return dispatch(setSelectedTrip(trip));
       })

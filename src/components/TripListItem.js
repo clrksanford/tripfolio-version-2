7904a956c82d9ac76908099fd67f6964d5ca074c
Clark Sanford <clrksanford@gmail.com>
2016-12-04 16:19:43 -0600

    Add logic to clean up URL with spaces

--- src/components/TripListItem.js
+++ src/components/TripListItem.js
@@ -9,6 +9,13 @@ class TripListItem extends Component {
   componentDidUpdate() {
     if(!_.isEmpty(this.props.selectedTrip)) {
       let { creatorUsername, destination } = this.props.selectedTrip;
+
+      // Clean up destination for display in URL
+      if(destination.indexOf(' ') !== -1) {
+        destination = destination.replace(/ /g, '_');
+      }
+
+      // Route user to completed trip page
       hashHistory.push(`completed/${creatorUsername}/${destination}`);
     }
   }

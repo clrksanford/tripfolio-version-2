e83f955f9b707b327e8ab6b8b38cc1cdb8a66868
Clark Sanford <clrksanford@gmail.com>
2016-12-04 22:07:19 -0600

    Rearrange reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,12 +6,12 @@ const setInfoToState = (state = {}, action) => {
       return { ...state, user: action.user };
     case 'SET_USER_TRIPS':
       return { ...state, userTrips: action.userTrips };
-    // case 'SET_FIREBASE':
-    //   return { ...state, firebase: action.firebase };
     case 'SET_SELECTED_TRIP':
       return Object.assign({}, state, {
         selectedTrip: action.selectedTrip
       });
+      // case 'SET_FIREBASE':
+      //   return { ...state, firebase: action.firebase };
     // case 'SET_CURRENT_TRIP_OWNER':
     //   return Object.assign({}, state, {
     //     ownerId: action.ownerId
@@ -20,8 +20,8 @@ const setInfoToState = (state = {}, action) => {
     //   return Object.assign({}, state, {
     //     destination: action.destination
     //   });
-    case 'SET_OWNER':
-      return { ...state, owner: action.owner };
+    // case 'SET_OWNER':
+    //   return { ...state, owner: action.owner };
     case 'SET_TRIPS_BY_DESTINATION':
       return { ...state, trips: action.trips };
     default:

d807af1a092afcf716afa66e1e9baf21fe827b90
Clark Sanford <clrksanford@gmail.com>
2016-12-04 22:47:19 -0600

    Add functionality for adding new form fields

--- src/components/TileEditor.js
+++ src/components/TileEditor.js
@@ -1,6 +1,40 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 class TileEditor extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formFields: [
+        <input type='text' placeholder='Title' ref='title'/>,
+        <textarea placeholder='Write some notes'></textarea>
+      ]
+    }
+
+    this._addField = this._addField.bind(this);
+  }
+
+  _addField(fieldName) {
+    let newField;
+
+    switch(fieldName) {
+      case 'openingHours':
+        newField = (
+          <input type='text' placeholder='Hours of operation'/>
+        );
+      default:
+        newField = '';
+    }
+
+    let newState = this.state.formFields;
+    newState.push(newField);
+
+    this.setState({
+      formFields: newState
+    });
+  }
+
   render() {
     return(
       <main>
@@ -9,13 +43,32 @@ class TileEditor extends Component {
           <div className='row'>
             <div className='column'>
               <form>
-                <input type='text' placeholder='Title' ref='title'/>
-                <textarea placeholder='Write some notes'></textarea>
+                {_.map(this.state.formFields, (field, index) => {
+                  return <div key={index}>{field}</div>;
+                })}
               </form>
             </div>
             <div className='column'>
               <h3>Add more fields</h3>
-              <a href='#'>Opening Hours</a>
+              <a href='#'
+                onClick={(e) => {
+                  e.preventDefault();
+                  let newField = (
+                    <input type='text' placeholder='Hours of operation'/>
+                  )
+
+                  let newState = this.state.formFields;
+                  newState.push(newField);
+
+                  this.setState({
+                    formFields: newState
+                  });
+
+                  console.log(this.state);
+                  console.log('new', newState);
+                }}>
+                  Opening Hours
+              </a>
               <a href='#'>Address</a>
               <a href='#'>Entrance Fee</a>
               <a href='#'>Transit/Parking</a>

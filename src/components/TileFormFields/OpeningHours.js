e5fd144d196235f4c58bdb8d1908e1540765efe8
Clark Sanford <clrksanford@gmail.com>
2016-12-05 22:05:28 -0600

    Redesign openinghours form

--- src/components/TileFormFields/OpeningHours.js
+++ src/components/TileFormFields/OpeningHours.js
@@ -4,21 +4,20 @@ class OpeningHours extends Component {
   render() {
     return(
       <form>
-        <select>
-          <option val='default'>Select a day</option>
-          <option val='0'>Sunday</option>
-          <option val='1'>Monday</option>
-          <option val='2'>Tuesday</option>
-          <option val='3'>Wednesday</option>
-          <option val='4'>Thursday</option>
-          <option val='5'>Friday</option>
-          <option val='6'>Saturday</option>
-        </select>
+        <input type='text' placeholder='Day(s)' />
         <div className='interval'>
-          <h4>From</h4>
-          <input type='text' />
-          <h4>To</h4>
-          <input type='text' />
+          <label>From</label>
+          <input type='text' />:<input type='text' />
+          <select>
+            <option value='AM'>AM</option>
+            <option value='PM'>PM</option>
+          </select>
+          <label>To</label>
+          <input type='text' />:<input type='text' />
+          <select>
+            <option value='AM'>AM</option>
+            <option value='PM'>PM</option>
+          </select>
         </div>
         <a href='#'>Add interval</a>
         <input type='checkbox'/> <label>Mark closed</label>

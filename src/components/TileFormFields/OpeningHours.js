6123b904639bf94fc420c0198ce6dc972bda10f5
Clark Sanford <clrksanford@gmail.com>
2016-12-06 10:48:10 -0600

    Begin stubbing out PUT request in OpeningHours

--- src/components/TileFormFields/OpeningHours.js
+++ src/components/TileFormFields/OpeningHours.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import _ from 'lodash';
 
 import Day from './Day';
@@ -15,6 +16,7 @@ class OpeningHours extends Component {
     }
 
     this._addDay = this._addDay.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
     this._removeDay = this._removeDay.bind(this);
   }
 
@@ -25,6 +27,24 @@ class OpeningHours extends Component {
     this.setState({
       dayArray
     });
+  }
+
+  _handleSubmit() {
+    let { tileId } = this.props;
+
+    let openingHours = {
+      
+    }
+
+    axios.put(`https://lit-garden-98394.herokuapp.com/travel-tiles/${tileId}`, {
+      openingHours
+    })
+      .then((response) => {
+        console.log(response);
+
+        // Close modal
+      })
+      .catch(err => console.log(err))
   }
 
   _removeDay() {
@@ -38,7 +58,10 @@ class OpeningHours extends Component {
 
   render() {
     return(
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        this._handleSubmit();
+      }}>
         {_.map(this.state.dayArray, (day, index) => {
           return <Day key={index} _removeDay={this._removeDay} />
         })}

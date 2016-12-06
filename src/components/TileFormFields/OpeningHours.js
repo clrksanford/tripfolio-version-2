7503d318047d28f6bf36aa58f24a4fba4aeaf7d9
Clark Sanford <clrksanford@gmail.com>
2016-12-05 22:27:42 -0600

    Refactor openinghours in terms of new components

--- src/components/TileFormFields/OpeningHours.js
+++ src/components/TileFormFields/OpeningHours.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
 
+import Day from './Day';
+
 class OpeningHours extends Component {
+
+
   render() {
     return(
       <form>
-        <input type='text' placeholder='Day(s)' />
-        <div className='interval'>
-          <label>From</label>
-          <input type='text' />:<input type='text' />
-          <select>
-            <option value='AM'>AM</option>
-            <option value='PM'>PM</option>
-          </select>
-          <label>To</label>
-          <input type='text' />:<input type='text' />
-          <select>
-            <option value='AM'>AM</option>
-            <option value='PM'>PM</option>
-          </select>
-        </div>
-        <a href='#'>Add interval</a>
-        <input type='checkbox'/> <label>Mark closed</label>
+        <Day />
       </form>
     );
   }

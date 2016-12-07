c622e09268b1ac5a0c722f093cd3b5be1231202a
Clark Sanford <clrksanford@gmail.com>
2016-12-06 23:04:45 -0600

    Add functionality for entrancefee editor

--- src/components/TileFormFields/Entrance.js
+++ src/components/TileFormFields/Entrance.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class Entrance extends Component {
   render() {
     return(
-      <form>
-        <label>Regular</label><input type='text' placeholder='price' />
-        <label>Student</label><input type='text' placeholder='price' />
+      <form onSubmit={this.props.handleSubmit(this.props.onSubmit)}>
+        <div className='entranceType'>
+          <Field name='entrance1label' component='input' type='text' placeholder='Regular' />
+          <Field name='entrance1price' component='input' type='text' />
+        </div>
+
+        <div className='entranceType'>
+          <Field name='entrance2label' component='input' type='text' placeholder='Student' />
+          <Field name='entrance2price' component='input' type='text' />
+        </div>
+
+        <div className='entranceType'>
+          <Field name='entrance3label' component='input' type='text' placeholder='Other (free hours)' />
+          <Field name='entrance3price' component='input' type='text' />
+        </div>
+
         <a href='#'>
           {/* Gives user a form to input a new discount type name and a new discount price (i.e. Under 25: $5) */}
           Add new discount type
         </a>
-        <a href='#'>
-          {/* This will add a textarea which lets user take down freeform notes, such as cutoff age for student discount or particular free hours */}
+        {/* <a href='#'>
+          This will add a textarea which lets user take down freeform notes, such as cutoff age for student discount or particular free hours
           Add notes
-        </a>
+        </a> */}
+
+        <button type='submit'>Save</button>
       </form>
     );
   }
 }
+
+Entrance = reduxForm({
+  form: 'entrance'
+})(Entrance);
 
 export default Entrance;

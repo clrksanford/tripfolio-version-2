be02cf1a5417231ef48474ab5413d6466f3bdc90
Clark Sanford <clrksanford@gmail.com>
2016-12-07 17:42:27 -0600

    Hook up Image form field to redux-form

--- src/components/TileFormFields/Image.js
+++ src/components/TileFormFields/Image.js
@@ -1,39 +1,21 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 import axios from 'axios';
 
 class Image extends Component {
-  constructor(props) {
-    super(props);
-
-    this._handleSubmit = this._handleSubmit.bind(this);
-  }
-
-  _handleSubmit() {
-    let { tileId } = this.props;
-    let imageURL = this.refs.imageURL.value;
-
-    axios.put(`https://lit-garden-98394.herokuapp.com/travel-tiles/${tileId}`, {
-      image: imageURL
-    })
-      .then((response) => {
-        console.log(response);
-
-        // Close modal
-      })
-      .catch(err => console.log(err))
-  }
-
   render() {
     return(
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        this._handleSubmit();
-      }}>
-        <input type='text' placeholder='Enter image URL' ref='imageURL' />
-        <input type='submit' value='Upload' />
+      <form onSubmit={this.props.handleSubmit(this.props.onSubmit)}>
+        <Field name='image' component='input' type='text'
+          placeholder='Enter image URL' />
+        <button type='submit'>Upload</button>
       </form>
     );
   }
 }
+
+Image = reduxForm({
+  form: 'image'
+})(Image);
 
 export default Image;

5f55f6fba49d287feb384a17e028ad2cc67ba8de
Clark Sanford <clrksanford@gmail.com>
2016-12-06 22:54:00 -0600

    Clean up address.js

--- src/components/TileFormFields/Address.js
+++ src/components/TileFormFields/Address.js
@@ -1,38 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import axios from 'axios';
 
 class Address extends Component {
-  // _handleSubmit() {
-  //   let tileId = this.props.tileId;
-  //
-  //   // Get user input
-  //   let street1 = this.refs.street1.value;
-  //   let street2 = this.refs.street2.value;
-  //   let city = this.refs.city.value;
-  //   let country = this.refs.country.value;
-  //   let zip = this.refs.zip.value;
-  //
-  //   // Compose options to send to DB
-  //   let address = {
-  //     street1,
-  //     street2,
-  //     city,
-  //     country,
-  //     zip
-  //   }
-  //
-  //   axios.put(`https://lit-garden-98394.herokuapp.com/travel-tiles/${tileId}`, {
-  //     address
-  //   })
-  //     .then((response) => {
-  //       console.log(response);
-  //
-  //       // Close modal
-  //     })
-  //     .catch(err => console.log(err))
-  // }
-
   render() {
     return(
       <form onSubmit={this.props.handleSubmit(this.props.onSubmit)}>

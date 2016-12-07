8a92228a9cc8dc1ee106d8801de83769e843189a
Clark Sanford <clrksanford@gmail.com>
2016-12-06 19:44:23 -0600

    Link up Transit component to submit info to redux-form

--- src/components/TileFormFields/Transit.js
+++ src/components/TileFormFields/Transit.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class Transit extends Component {
   render() {
     return(
-      <form>
-        <textarea placeholder='Notes on transit or parking'></textarea>
+      <form onSubmit={this.props.handleSubmit(this.props.onSubmit)}>
+        <Field name='transitNotes' component='textarea' placeholder='Notes on transit or parking' />
         <a href='#'>
           {/* Lets user add a new link to sites with more info; import TileLinks for this */}
           Add Link
         </a>
+        <button type='submit'>Save</button>
       </form>
     );
   }
 }
+
+Transit = reduxForm({
+  form: 'transit'
+})(Transit);
 
 export default Transit;

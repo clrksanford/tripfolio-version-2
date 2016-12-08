84c1285557963c23afa063f0da3b21036dad6db6
Clark Sanford <clrksanford@gmail.com>
2016-12-08 15:40:09 -0600

    Fix rendering of OpeningHours

--- src/components/CompletedTileFields/OpeningHours.js
+++ src/components/CompletedTileFields/OpeningHours.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import _ from 'lodash';
 
+import deleteEmptyKeys from '../../constants/deleteEmptyKeys';
+
 export default (props) => {
   let { openingHours } = props.tile;
-  
+
   return(
     <div className='row completedTileContainer'>
       <div className='tileHeader'>
@@ -12,17 +14,27 @@ export default (props) => {
       <div className='tileBody'>
         <ul>
           {_.map(openingHours, (hoursObj, index) => {
-            _.map(hoursObj, (value, key) => {
-              if(!_.isNil(value)) {
-                return(
-                  <li key={index}>
-                    <p>{hoursObj.value}: {hoursObj.from1}-{hoursObj.to1}</p>
-                    <p>{hoursObj.from2}-{hoursObj.to2}</p>
-                    <p>{hoursObj.from3}-{hoursObj.to3}</p>
-                  </li>
-                )
-              }
-            })
+            let interval1, interval2, interval3;
+
+            if(!_.isNil(hoursObj.from1) && !_.isNil(hoursObj.to1)) {
+              interval1 = `${hoursObj.from1}-${hoursObj.to1}`;
+            }
+
+            if(!_.isNil(hoursObj.from2) && !_.isNil(hoursObj.to2)) {
+              interval2 = `${hoursObj.from2}-${hoursObj.to2}`;
+            }
+
+            if(!_.isNil(hoursObj.from3) && !_.isNil(hoursObj.to3)) {
+              interval3 = `${hoursObj.from3}-${hoursObj.to3}`;
+            }
+
+            return(
+              <li key={index}>
+                <p>{hoursObj.value}: {interval1}</p>
+                <p>{interval2}</p>
+                <p>{interval3}</p>
+              </li>
+            )
           })}
         </ul>
       </div>

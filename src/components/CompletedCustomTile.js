a8addd705b5334e504baca25a0a9933b26a74361
Clark Sanford <clrksanford@gmail.com>
2016-12-08 10:03:03 -0600

    Add bootstrap classnames to CompletedCustomTile

--- src/components/CompletedCustomTile.js
+++ src/components/CompletedCustomTile.js
@@ -4,72 +4,82 @@ class CompletedCustomTile extends Component {
   render() {
     return(
       <div className='completedCustomTile'>
-        <div className='pageHeader'>
-          <h3>Tile.Name</h3>
-        </div>
-        <div className='row'>
-          <img src='#' alt='tile.image' />
-        </div>
-        <div className='row'>
-          <p>Tile.notes</p>
-        </div>
-        <div className='row'>
-          <h4>Helpful Links</h4>
-          <ul>
-            <li>Link1</li>
-            <li>Link2</li>
-          </ul>
-        </div>
-        <div className='row'>
-          <div className='column'>
-            <div className='row completedTileContainer'>
-              <h4>Address</h4>
-              {/* <a href='#' onClick={(e) => {
-                e.preventDefault();
-                this._showFieldModal('address');
-              }}> */}
-                <p>
-                  street1,
-                  city, country
-                  zip
-                </p>
-              {/* </a> */}
-            </div>
-            <div className='row completedTileContainer'>
-              <h4>Hours</h4>
-              {/* <a href='#' onClick={(e) => {
-                e.preventDefault();
-                this._showFieldModal('openingHours');
-              }}> */}
-                <p>
-                  day1, day2, from1, to1, etc...
-                </p>
-              {/* </a> */}
-            </div>
+        <div className='container'>
+          <div className='row'>
+            <h2>Tile.Name</h2>
           </div>
-          <div className='column'>
-            <div className='row completedTileContainer'>
-              <h4>Admissions</h4>
-              {/* <a href='#' onClick={(e) => {
-                e.preventDefault();
-                this._showFieldModal('entrance');
-              }}> */}
-                <p>
-                  entrance type 1
-                </p>
-                <p>
-                  entrance type 2
-                </p>
-                <p>
-                  entrance type 3
-                </p>
-              {/* </a> */}
+        </div>
+        <div className='container'>
+          <div className='row'>
+            <img src='#' alt='tile.image' />
+          </div>
+        </div>
+        <div className='container'>
+          <div className='row'>
+            <p>Tile.notes</p>
+          </div>
+        </div>
+        <div className='container'>
+          <div className='row'>
+            <h4>Helpful Links</h4>
+            <ul>
+              <li>Link1</li>
+              <li>Link2</li>
+            </ul>
+          </div>
+        </div>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6'>
+              <div className='row completedTileContainer'>
+                <h4>Address</h4>
+                {/* <a href='#' onClick={(e) => {
+                  e.preventDefault();
+                  this._showFieldModal('address');
+                }}> */}
+                  <p>
+                    street1,
+                    city, country
+                    zip
+                  </p>
+                {/* </a> */}
+              </div>
+              <div className='row completedTileContainer'>
+                <h4>Hours</h4>
+                {/* <a href='#' onClick={(e) => {
+                  e.preventDefault();
+                  this._showFieldModal('openingHours');
+                }}> */}
+                  <p>
+                    day1, day2, from1, to1, etc...
+                  </p>
+                {/* </a> */}
+              </div>
             </div>
-            <div className='row completedTileContainer'>
-              <h4>Transit/Parking</h4>
-              <p>
-                transit notes
-              </p>
+            <div className='col-md-6'>
+              <div className='row completedTileContainer'>
+                <h4>Admissions</h4>
+                {/* <a href='#' onClick={(e) => {
+                  e.preventDefault();
+                  this._showFieldModal('entrance');
+                }}> */}
+                  <p>
+                    entrance type 1
+                  </p>
+                  <p>
+                    entrance type 2
+                  </p>
+                  <p>
+                    entrance type 3
+                  </p>
+                {/* </a> */}
+              </div>
+              <div className='row completedTileContainer'>
+                <h4>Transit/Parking</h4>
+                <p>
+                  transit notes
+                </p>
+              </div>
             </div>
           </div>
         </div>

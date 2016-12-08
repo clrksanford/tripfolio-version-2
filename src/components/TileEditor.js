184306a466412c7a391017bfaaed20cb84749d30
Clark Sanford <clrksanford@gmail.com>
2016-12-08 15:18:25 -0600

    Extract deleteEmptyKeys function to constants folder

--- src/components/TileEditor.js
+++ src/components/TileEditor.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 import _ from 'lodash';
 
+import deleteEmptyKeys from '../constants/deleteEmptyKeys';
+
 import TileEditorModal from './TileEditorModal';
 import Address from './TileFormFields/Address';
 import CompletedCustomTile from './CompletedCustomTile';
@@ -295,29 +297,6 @@ class TileEditor extends Component {
         name: link6Name,
         url: link6URL
       }
-    }
-
-    function deleteEmptyKeys(obj) {
-      for (var key in obj) {
-        // console.log('key', key);
-        // console.log('value', obj[key]);
-        if(_.isNil(obj[key]) ||
-          typeof obj[key] === 'object' && _.isEmpty(obj[key])) {
-
-          // console.log(`${obj[key]} deleted`);
-          delete obj[key];
-        } else if (typeof obj[key] === 'object') {
-          // console.log('going around again');
-          deleteEmptyKeys(obj[key]);
-
-          // If this embedded object is now empty, delete it
-          if(_.isEmpty(obj[key])) {
-            // console.log('now the embedded obj is empty too; delete it');
-            delete obj[key];
-          }
-        }
-      }
-      return obj;
     }
 
     address = deleteEmptyKeys(address);
